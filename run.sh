29308c5c235ef4673db872a65587c88a29623de7
mubius <79933806+glados-creator@users.noreply.github.com>
2024-05-28 08:56:36 +0200

    .sh pour exe

---
+++ run.sh
@@ -0,0 +1,3 @@
+clear
+javac -d ./bin/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls ./src/*.java
+java  -cp ./bin/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls Executable
---
+++ tp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+clear
+mkdir bin
+echo "compiling"
+javac -d ./bin/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls ./src/*.java
+echo "" &&
+mkdir doc
+echo "doc" &&
+javadoc -d ./doc/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls ./src/*.java &&
+echo "" &&
+echo "running" &&
+echo "" &&
+java -cp ./bin/ --module-path /usr/share/openjfx/lib/ --add-modules javafx.controls Executable
