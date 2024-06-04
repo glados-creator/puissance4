4b93edbeaaeaddc61fbf91ba8c55cb3c68f685a3
glados-creator <79933806+glados-creator@users.noreply.github.com>
2024-06-04 09:30:23 +0200

    model fini

--- src/model.java
+++ src/model.java
@@ -1,10 +1,4 @@
 public class model {
-    public enum ending {
-        running,
-        lose,
-        win;
-    }
-
     private static final short MIN_WIDTH = 5;
     private static final short MAX_WIDTH = 16;
     private static final short MIN_HEIGHT = 5;
@@ -16,13 +10,13 @@ public class model {
     private short iheight;
     private short nbequipe;
     private short[][] iboard;
-    private ending state;
+    private short state;
 
     public void create_model(short width, short height, short n_nbequipe) {
         nbequipe = (short) n_nbequipe;
         iwidth = width;
         iheight = height;
-        state = ending.running;
+        state = -1;
         iboard = new short[width][height];
         for (short i = 0; i < iwidth; i++) {
             for (short j = 0; j < iheight; j++) {
@@ -64,39 +58,74 @@ public class model {
         return true;
     }
 
-    public short get(short width, short height) {
+    public boolean inbound(short width, short height) {
         if (width < 0 || width > iwidth)
-            return -1;
+            return false;
         if (height < 0 || height > iheight)
+            return false;
+        return true;
+    }
+
+    public short get(short width, short height) {
+        if (!(inbound(width, height)))
             return -1;
         return iboard[width][height];
     }
 
     public boolean set(short width, short height, short equipe) {
-        if (width < 0 || width > iwidth)
-            return false;
-        if (height < 0 || height > iheight)
+        if (!(inbound(width, height)))
             return false;
         if (iboard[width][height] != 0)
             return false;
         iboard[width][height] = equipe;
-        check();
+        state = check();
         return true;
     }
 
-    private void check() {
-        ; // TODO : code
+    private short check() {
+        short nb = 0;
+        short last = -1;
+        for (short i = 0; i < iwidth && state == -1; i++) {
+            for (short j = 0; j < iheight && state == -1; j++) {
+                // for each case
+                for (short dx = -1; dx < 2 && state == -1; dx++) {
+                    for (short dy = -1; dy < 2 && state == -1; dy++) {
+                        // for each direction
+                        last = -1;
+                        nb = 0;
+                        for (short h = 0; h < 4; h++) {
+                            // 4 times
+                            short c_w = (short)(i+(dx*h));
+                            short c_h = (short)(j+(dy*h));
+                            if (!(inbound(c_w,c_h)))
+                                break;
+                            if (last == -1 || last == iboard[c_w][c_h]) {
+                                if (last == -1) {
+                                    last = iboard[c_w][c_h];
+                                } else {
+                                    nb += 1;
+                                }
+                            } else {
+                                last = iboard[c_w][c_h];
+                                nb = 1;
+                            }
+                        }
+                        if (nb > 3) {
+                            state = last;
+                        }
+
+                    }
+                }
+            }
+        }
+        return -1;
     }
 
     public boolean win() {
-        return ending.win == state;
+        return state != -1;
     }
 
-    public boolean lose() {
-        return ending.lose == state;
-    }
-
-    public ending getstate() {
+    public short get_equipe_win() {
         return state;
     }
 
